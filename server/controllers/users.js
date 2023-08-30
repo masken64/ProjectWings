972c34cb4e61f4161f1b694dff0812742fd8c8fa
Kartik Sharma <66724218+masken64@users.noreply.github.com>
2023-08-30 14:52:29 +0530

    friendlist logic

---
+++ server/controllers/users.js
@@ -0,0 +1,65 @@
+import User from "../models/User";
+//read
+export const getUser = async(req,res)=>{
+    try{
+        const { id } = req.params;
+        const user = await User.findById(id);
+        res.status(200).json(user);
+    }catch(err){
+        res.status(404).json({message : err.message});
+    }
+}
+
+export const getUserFriends = async (req,res) =>{
+    try{
+        const { id } = req.params;
+        const user = await User.findById(id);
+        
+        const friends = await Promise.all(
+            user.friends.map((id) => User.findById(id))
+        );
+        const formattedFriends = friends.map(
+            ({ _id, firstName, lastName, disability, location, picturePath}) => {
+                return { _id, firstName, lastName, disability, location, picturePath };
+            }
+        );
+        res.status(200).json(formattedFriends);
+    }catch(err){
+        res.status(404).json({message : err.message});
+   
+    }
+};
+
+//update friends list
+export const addRemoveFriend = async(req, res) => {
+    try{
+        const {is, friendId } = req.params;
+        const user = await User.findById(id);
+        const friend = await User.findById(friendId);
+
+        if(user.friends.includes(friendId)) {
+            user.friends = user.friends.filter((id) => id!== friendId);
+            friend.friends = friend.friends.filter((id) => id !== id);
+        } else {
+            user.friends.push(friendId);
+            friend.friends.push(id);
+        }
+
+        await user.save();
+        await friend.save();
+
+        const friends = await Promise.all(
+            user.friends.map((id) => User.findById(id))
+        );
+        const formattedFriends = friends.map(
+            ({ _id, firstName, lastName, disability, location, picturePath}) => {
+                return { _id, firstName, lastName, disability, location, picturePath };
+            }
+        );
+
+        res.status(200).json(formattedFriends);
+    } catch(err){
+        res.status(404).json({message : err.message});
+   
+    }
+};
--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js";
+import userRoutes from "./routes/users.js";
 import { register } from "./controllers/auth.js";
 
 //configs(middleware)
@@ -42,6 +43,7 @@ app.post("/auth/register", upload.single("picture"), register); //uploads to pub
 
 //routes
 app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
 
 //mongoose setup
 const PORT = process.env.PORT || 6001;
